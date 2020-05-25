782594fa71c2f313dcd4150b72bb1346dad0ad87
sujathaM518 <sujatha@macsof.com>
2020-05-25 16:05:49 +0530

    Initial commit

---
+++ src/app/views/sessions/signin/signin.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit, ViewChild, OnDestroy, AfterViewInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { JwtAuthService } from '../../../shared/services/auth/jwt-auth.service';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild(MatProgressBar) progressBar: MatProgressBar;
+  @ViewChild(MatButton) submitButton: MatButton;
+
+  signinForm: FormGroup;
+  errorMsg = '';
+  return: string;
+
+  private _unsubscribeAll: Subject<any>;
+
+  constructor(
+    private jwtAuth: JwtAuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this._unsubscribeAll = new Subject();
+  }
+
+  ngOnInit() {
+    this.signinForm = new FormGroup({
+      username: new FormControl('Watson', Validators.required),
+      password: new FormControl('12345678', Validators.required),
+      rememberMe: new FormControl(true)
+    });
+
+    this.route.queryParams
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe(params => this.return = params['return'] || '/');
+  }
+
+  ngAfterViewInit() {
+    
+  }
+
+  ngOnDestroy() {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
+
+  signin() {
+    const signinData = this.signinForm.value
+
+    this.submitButton.disabled = true;
+    this.progressBar.mode = 'indeterminate';
+    console.log('sign');
+    
+    this.jwtAuth.signin(signinData.username, signinData.password)
+    .subscribe(response => {
+      this.router.navigateByUrl(this.return);
+    }, err => {
+      this.submitButton.disabled = false;
+      this.progressBar.mode = 'determinate';
+      this.errorMsg = err.message;
+      // console.log(err);
+    })
+  }
+
+}
